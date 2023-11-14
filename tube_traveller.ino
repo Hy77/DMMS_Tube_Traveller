0bbd1588a4ba8137d1c465b863a7607703996a99
Hy77 <74601064+Hy77@users.noreply.github.com>
2023-11-14 16:39:46 +1100

    Update tube_traveller.ino

--- tube_traveller.ino
+++ tube_traveller.ino
@@ -9,6 +9,10 @@ const int servo_top2 = 1;
 const int servo_under1 = 2;
 const int servo_under2 = 3;
 
+// fake delay
+unsigned long currentTime = 0; // cur time
+unsigned long previousTime = 0; // pre time
+
 void setup() {
   Serial.begin(9600); // serial port
   pwm.begin();
@@ -31,17 +35,36 @@ void loop() {
       case 'e':
         rotateClockwise();
         break;
+      default:
+        pwm.writeMicroseconds(servo_top1,0);
+        pwm.writeMicroseconds(servo_top2,0);
+        pwm.writeMicroseconds(servo_under1,0);
+        pwm.writeMicroseconds(servo_under2,0);
+        break;
     }
   }
+}
+
+void fakeDelay(int delayTime) {
+  currentTime = millis(); // get cur time
+
+  // 如果距离上一次旋转已经超过指定的延迟时间，就返回
+  if (currentTime - previousTime >= delayTime) {
+    previousTime = currentTime; // update pre time
+    return;
+  }
+  // wait if...
+  while (currentTime - previousTime < delayTime) {
+    currentTime = millis(); // update cur
+  }
+  previousTime = currentTime; // update pre
 }
 
 void moveForward() {
   // moving forward
   Serial.println('w');
-  pwm.setPWM(servo_top1, 0, 4096);
-  pwm.setPWM(servo_top2, 0, 4096);
-  pwm.setPWM(servo_under1, 0, 4096);
-  pwm.setPWM(servo_under2, 0, 4096);
+  pwm.writeMicroseconds(servo_top1,2500);
+  fakeDelay(3000);
 }
 
 void moveBackward() {
